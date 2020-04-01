fb9773d0415a595993cb854e18e2f31ce66dbe8f
Glitch (sitepoint-editors-discord-bot-sitepoint-2) <none>
2020-04-01 05:35:11 +0000

    🚟😇 Checkpoint ./index.js:2922321/316

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ bot.on('message', msg => {
     if (msg.content.includes("I am over 18 years old")) {
       if(!generatedCodes[msg.author]){
         const code = md5(msg.author);
-        msg.reply(`your code is \`${code}\`, please keep it save and secret.`);
+        msg.reply(`your code is \`${code}\`, please keep it safe and secret.`);
         generatedCodes[msg.author] = {
           code: code,
           requests: 1
@@ -25,16 +25,21 @@ bot.on('message', msg => {
       }else{
         const obj = generatedCodes[msg.author];
         obj.requests++;
-        msg.reply(`here is your code again: \`{obj.code}\`. You requested it {obj.requests} times. Please remember it, I'm not your mother!`)
+        msg.reply(`here is your code again: \`${obj.code}\`. You requested it ${obj.requests} times. Please remember it, I'm not your mother!`)
       }
     } else {
       msg.reply('please specify `I am over 18 years old` to retrieve your token!');
     }
-  } else if(msg.content.startWith('!stats')){
-    const users = generatedCodes.keys()
-    const requests = _.sumBy(generatedCodes.values(), obj => {
+  } else if(msg.content.startsWith('!stats')){
+    const users = Object.keys(generatedCodes).length;
+    const requests = _.sumBy(Object.values(generatedCodes), obj => {
       return obj.requests;
     })
-    msg.channel.send(`I generated [`)
+    msg.channel.send(`I generated ${requests} code(s) for ${users} different user(s).`)
+  } else if(msg.content.startsWith('!export')){
+    const codes = _.map(generatedCodes, obj => {
+      return obj.code;
+    })
+    const 
   }
 });
