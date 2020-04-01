a67bbf3519ce20ea93c86ed71e13ec37c772454d
Glitch (sitepoint-editors-discord-bot-sitepoint-2) <none>
2020-04-01 05:13:07 +0000

    🛥🚋 Checkpoint ./index.js:2922321/245 ./package.json:2922321/29

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const Discord = require('discord.js');
+const md5 = require('blueimp-md5');
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 
@@ -11,11 +12,11 @@ bot.on('ready', () => {
 
 bot.on('message', msg => {
   if (msg.content.startsWith('!code')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
+    if (msg.content.includes("I am over 18 years old")) {
+      const code = md5(msg.mentions.users.first());
       msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
     } else {
-      msg.reply('Please tag a valid user!');
+      msg.reply('please specify `I am over 18 years old` to retrieve your token!');
     }
   }
 });
